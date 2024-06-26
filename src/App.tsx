7d7971e51955808640a729a853b6dcf3c0b0cb7b
vishavsingla <vishav9933@gmail.com>
2024-06-27 00:17:33 +0530

    basic maze generator component made and working

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+import MazeGenerator from './components/MazeGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-2xl font-bold mb-4 p-4">Maze Generator</h1>
+      <MazeGenerator />
+    </div>
+  );
+}
+
+export default App;
